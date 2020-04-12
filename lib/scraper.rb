5d652c745c08bfe4ec58d6af2ef36ccbac346b58
Kevin Mathias <komathias1@gmail.com>
2020-04-12 20:11:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student_card 1")
+    student_cards.collect do |card|
+      {:name => card.css(".student_name").text,
+      :location => card.css(".student_location").text,
+      :profile_url => card.attr('href')
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
